57840aefc609bffbedc2fe46fa16df8a6da6d3cc
Sem Saz <66939700+SemaSaz@users.noreply.github.com>
2025-05-04 15:30:26 +0300

    Add files via upload

---
+++ Beta_0_1_1/Game/lib/Check/GameController.cpp
@@ -0,0 +1,8 @@
+#include "GameController.h"
+#include <Arduino.h>
+
+// void GameController::processInput() {
+//     JoystickPosition currentPosition = pollJoystick();
+//     Serial.println(joystickPositionToString(currentPosition));
+
+// }
---
+++ Beta_0_1_1/Game/src/init_master.h
@@ -0,0 +1,10 @@
+#ifndef init_master_h
+#define init_master_h
+
+#include <Arduino.h>
+
+#include "init_master/init_classes.h"
+#include "init_master/init_function.h"
+#include "init_master/init_variables.h"
+
+#endif
